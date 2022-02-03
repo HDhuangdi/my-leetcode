d23b7ff78f1133dc57a4bfa86a10e40c2e6fbdc1
huangdi <1013697816@qq.com>
2022-02-03 16:25:36 +0800

    #509

--- 动态规划/#509 斐波那契数/1.js
+++ 动态规划/#509 斐波那契数/1.js
@@ -1,10 +1,8 @@
 var fib = function (n) {
+  if (n < 2) return n;
   let l = 0;
   let r = 1;
   let a = 1;
-  if (n === 0) return l;
-  if (n === 1) return r;
-  if (n === 2) return a;
   for (let i = 2; i < n; i++) {
     l = r;
     r = a;
