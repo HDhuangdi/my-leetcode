0d6696a09e23eaba30ec225a07e4208df0eabee4
huangdi <1013697816@qq.com>
2022-02-16 17:47:30 +0800

    #322

---
+++ 动态规划/#322 零钱兑换/1.js
@@ -0,0 +1,14 @@
+// dp 数组法
+
+var coinChange = function (coins, amount) {
+  let dp = new Array(amount + 1).fill(Number.MAX_VALUE);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      let coin = coins[j];
+      if (i < coin) continue;
+      dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+    }
+  }
+  return dp[dp.length - 1] === Number.MAX_VALUE ? -1 : dp[dp.length - 1];
+};
