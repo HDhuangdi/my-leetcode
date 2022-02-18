9df07a6c06e381879c25e6c9c0393bd86bfb1279
huangdi <1013697816@qq.com>
2022-02-18 17:44:18 +0800

    #152 笔记补充

--- 动态规划/#152 乘积最大子数组/1.js
+++ 动态规划/#152 乘积最大子数组/1.js
@@ -6,8 +6,10 @@ var maxProduct = function (nums) {
   let max = [nums[0]];
 
   for (let i = 1; i < nums.length; i++) {
-    min[i] = Math.min(min[i - 1] * nums[i], max[i - 1] * nums[i], nums[i]);
-    max[i] = Math.max(min[i - 1] * nums[i], max[i - 1] * nums[i], nums[i]);
+    let minProduct = min[i - 1] * nums[i];
+    let maxProduct = max[i - 1] * nums[i];
+    min[i] = Math.min(minProduct, maxProduct, nums[i]);
+    max[i] = Math.max(minProduct, maxProduct, nums[i]);
     dp[i] = Math.max(max[i], dp[i - 1]);
   }
 
