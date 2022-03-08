5241b2f2904eb12c0a44ca13366dd83b391477cf
huangdi <1013697816@qq.com>
2022-03-08 11:13:36 +0800

    no message

--- 动态规划/#5 最长回文子串/1.js
+++ 动态规划/#5 最长回文子串/1.js
@@ -6,6 +6,8 @@ var longestPalindrome = function (s) {
   for (let i = 0; i < len; i++) {
     dp[i][i] = true;
   }
+  // 这里遍历的顺序是有讲究的
+  // 由于状态是从短串转移到长串的,所以需要先把短串的状态全部确定下来
   for (let L = 2; L <= len; L++) {
     for (let i = 0; i < len; i++) {
       // j - i + 1 = L
