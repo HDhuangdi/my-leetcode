a128183baed2cb400bc3a861398def247dd05cf5
huangdi <1013697816@qq.com>
2022-03-24 22:51:30 +0800

    #139

--- 动态规划/#139 单词拆分/2.js
+++ 动态规划/#139 单词拆分/2.js
@@ -1,26 +1,22 @@
-// 二维dp
+// 一维dp
 
 var wordBreak = function (s, wordDict) {
-  const dp = new Array(wordDict.length + 1)
-    .fill(0)
-    .map(() => new Array(s.length + 1).fill(false));
+  const dp = new Array(s.length + 1).fill(false);
   // 当s的长度为0时，怎么样都可以拼成
   for (let i = 0; i <= wordDict.length; i++) {
-    dp[i][0] = true;
+    dp[0] = true;
   }
   for (let i = 1; i <= wordDict.length; i++) {
     for (let j = 0; j <= s.length; j++) {
       for (let word of wordDict.slice(0, i)) {
+        if (dp[j]) break;
         if (word.length > j) continue;
         const _s = s.slice(j - word.length, j);
         if (_s === word) {
-          dp[i][j] = dp[i - 1][j] || dp[i][j - word.length];
-        } else {
-          dp[i][j] = dp[i - 1][j];
+          dp[j] = dp[j] || dp[j - word.length];
         }
-        if (dp[i][j]) break;
       }
     }
   }
-  return dp[wordDict.length][s.length];
+  return dp[s.length];
 };
